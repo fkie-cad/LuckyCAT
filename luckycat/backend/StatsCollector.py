187933f4bc3124597fcf3f9d2e1c6234add81ff2
0roman <roman.wagner18@gmx.de>
2018-09-06 16:16:59 +0200

    general stats collector fixed

--- luckycat/backend/StatsCollector.py
+++ luckycat/backend/StatsCollector.py
@@ -4,6 +4,7 @@ import json
 import datetime
 from multiprocessing import Process
 from mongoengine import connect
+from mongoengine.queryset import DoesNotExist
 from luckycat import f3c_global_config
 from luckycat.backend import WorkQueue
 from luckycat.database.models.Statistic import Statistic
@@ -21,6 +22,16 @@ class StatsCollector(Process):
         self.channel = self.wq.get_channel()
 
     def update_stats(self, stats):
+        try:
+            current_stats = Statistic.objects.get(job_id=stats['job_id'])
+            current_stats.update(set__iteration=current_stats['iteration'] + 1)
+        except DoesNotExist:
+            current_stats = Statistic(job_id=str(stats['job_id']),
+                                      runtime=0,
+                                      iteration=1,
+                                      execs_per_sec=0,
+                                      date=datetime.datetime.now())
+            current_stats.save()
         # TODO get last iteration and add one
         # current_stats = Statistic(job_id=str(stats['job_id']),
         #                           runtime=0,
