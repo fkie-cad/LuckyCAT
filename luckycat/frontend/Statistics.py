3290e103e38eea196efe4e495b32117b0281955d
0roman <roman.wagner18@gmx.de>
2018-09-14 17:46:19 +0200

    fixed general view for selected project

--- luckycat/frontend/Statistics.py
+++ luckycat/frontend/Statistics.py
@@ -10,6 +10,20 @@ statistics = flask.Blueprint('statistics', __name__)
 # TODO use SQLalchemy
 class StatisticCalculator:
 
+    def calculate_statistics(self):
+        statistic = {}
+        selected_project = self.get_selected_project()
+        job_names = self.list_job_names()
+        project_statistics = {"job_names": job_names}
+        if selected_project:
+            project_statistics.update(self.calculate_general_statistics_for_specific_project(selected_project))
+            statistic["project_statistics"] = project_statistics
+        else:
+            project_statistics = self.calculate_general_statistics(project_statistics)
+            statistic["project_statistics"] = project_statistics
+        statistic["test"] = "test"
+        return statistic
+
     def summarize_individual_project_statistics(self):
         iteration = 0
         runtime = 0
@@ -20,21 +34,42 @@ class StatisticCalculator:
             execs_per_sec += project_statistic.execs_per_sec
         return {"iteration": iteration, "runtime": runtime, "execs_per_sec": execs_per_sec}
 
-    def list_job_names():
+    def list_job_names(self):
         job_names = []
         for job in Job.objects:
             job_names.append(job.name)
         return job_names
 
-    def calculate_statistics(self):
-        statistic = {}
-        project_statistics = self.summarize_individual_project_statistics()
-        job_names = self.list_job_names()
+    def calculate_general_statistics(self, project_statistics):
+        project_statistics.update(self.summarize_individual_project_statistics())
         number_of_job_names = Job.objects.count()
-        statistic["project_statistics"] = project_statistics
-        # project_query = web.input()
-        project_query = flask.request.args.get("project")
-        return statistic
+        project_statistics["number_of_job_names"] = number_of_job_names
+        return project_statistics
+
+    def calculate_general_statistics_for_specific_project(self, selected_project):
+        # what to do if two projects have the same name. which one should be selected?
+        project_statistic = Statistic.objects.aggregate(*[
+            {
+                '$lookup':
+                    {'from': Job._get_collection_name(),
+                     'localField': 'job_id',
+                     'foreignField': '_id',
+                     'as': 'relation'}
+            },
+            {
+                "$match": {"relation.name": selected_project}
+            },
+        ])
+        project_statistic = self.transform_CommandCursorObject_to_dict(project_statistic)
+        return {"iteration": project_statistic["iteration"],
+                "runtime": project_statistic["runtime"],
+                "execs_per_sec": project_statistic["execs_per_sec"]}
+
+    def get_selected_project(self):
+        return flask.request.args.get("project")
+
+    def transform_CommandCursorObject_to_dict(self, project_statistic):
+        return list(project_statistic)[0]
 
 
 @statistics.route('/statistics/show', methods=['GET'])
