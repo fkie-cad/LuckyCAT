b6c00b77a82e81ec30e1a32bb86758e310ec1653
krah <krah@protonmail.com>
2018-10-05 10:34:46 +0200

    refactoring freeing of ports

--- start_cluster.sh
+++ start_cluster.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
+pidkill () {
+    echo '[*] Found PID occupying port :'$1' - pid '$2
+    echo '[*] Attempting to kill pid '$2
+    sudo kill $2
+    if [[ $? -eq '0' ]]; then
+        echo '[+] Success!'
+    else
+        echo '[!] Failed to free process: '$2
+        echo '[!] Aborting Lucky CAT startup'
+        exit 1
+    fi
+}
+
 for port in '5000' '5672'; do
     pid=$(sudo lsof -i :$port | grep LISTEN | grep  '[0-9]*' | cut -d' ' -f2)
     if [[ $pid -ne '0' ]]; then
-        echo '[*] Found PID occupying port :'$port' - pid '$pid
-        echo '[*] Attempting to kill pid '$pid
-        sudo kill $pid
-        if [[ $? -eq '0' ]]; then
-            echo '[+] Success!'
-        else
-            echo '[!] Failed to free process: '$pid
-            echo '[!] Aborting Lucky CAT startup'
-            exit 1
-        fi
+        pidkill $port $pid
+    fi
+done
+
+for port in '27017'; do
+    pid=$(sudo lsof -i :$port | grep LISTEN | grep  '[0-9]*' | cut -d' ' -f3)
+    if [[ $pid -ne '0' ]]; then
+        pidkill $port $pid
     fi
 done
 
