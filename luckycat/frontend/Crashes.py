91527fc6df6274859748a0a3ab2ddf53e58efad4
Christopher Krah <christopher.krah@fkie.fraunhofer.de>
2019-12-09 14:22:47 +0100

    fixed crash search

--- luckycat/frontend/Crashes.py
+++ luckycat/frontend/Crashes.py
@@ -54,12 +54,12 @@ def show_crashes(crashes=None):
     job_ids = _get_job_ids_of_user()
     sorted_res = collections.defaultdict(list)
     for crash in crashes:
-        if crash["job_id"] in job_ids:
-            sorted_res[str(crash["job_id"])] = sorted_res[str(crash["job_id"])] + [crash]
+        if crash['job_id'] in job_ids:
+            sorted_res[str(crash['job_id'])] = sorted_res[str(crash['job_id'])] + [crash]
     final_res = collections.defaultdict(list)
     for k, v in sorted_res.items():
         final_res[_get_job_name_from_id(k)] = v
-    return render_template("crashes_show.html",
+    return render_template('crashes_show.html',
                            results=final_res.items())
 
 
@@ -75,16 +75,16 @@ def show_crash(crash_id):
             if crash.job_id in job_ids:
                 job_name = get_job_name_of_job_id(crash.job_id)
                 encoded_original_test_case, encoded_crash_test_case = get_original_and_crash_test_case_of_crash(crash)
-                return render_template("crashes_view.html",
+                return render_template('crashes_view.html',
                                        crash=crash,
                                        job_name=job_name,
                                        encoded_crash_test_case=encoded_crash_test_case,
                                        encoded_original_test_case=encoded_original_test_case)
             else:
-                flash("'Not allowed to view this crash.")
+                flash('Not allowed to view this crash.')
                 return redirect('crashes/show')
         else:
-            flash('No crash with crash id %s' % crash_id)
+            flash(f'No crash with crash id {crash_id}')
             return redirect('crashes/show')
     else:
         flash('Not a valid crash id.')
@@ -92,13 +92,14 @@ def show_crash(crash_id):
 
 
 def get_job_name_of_job_id(job_id):
-    return list(Job.objects(id=job_id))[0]["name"]
+    return list(Job.objects(id=job_id))[0]['name']
 
 
 @crashes.route('/crashes/search', methods=['GET', 'POST'])
 @login_required
-def search_crash(error=None):
-    crash_database_structure = [item for item in Crash._get_collection().find()[0]]
+def search_crash():
+    crash_database_structure, error = _getCrashStructureFromDB()
+
     job_names = _get_job_names_of_user()
     if request.method == 'POST':
         try:
@@ -106,27 +107,36 @@ def search_crash(error=None):
             if crashes:
                 return show_crashes(crashes=crashes)
             else:
-                error = "No Crashes are fitting to your Search Request"
+                error = 'No crashes found!'
         except Exception as e:
             error = e
 
-    return render_template("crashes_search.html", database_structure=crash_database_structure, job_names=job_names,
+    return render_template('crashes_search.html',
+                           database_structure=crash_database_structure,
+                           job_names=job_names,
                            error=error)
+
+
+def _getCrashStructureFromDB():
+    try:
+        return [item for item in Crash._get_collection().find()[0]]
+    except IndexError:
+        error = 'Database is empty!'
+        return {}, error
 
 
 def process_search_query():
     query = json.loads(request.form['search'])
     selected_job_name = request.form.get('job')
-    if not selected_job_name == "":
+    if not selected_job_name == '':
         job_id = _get_job_id_of_job_name(selected_job_name)
-        query.update({"job_id": job_id})
-    crashes = Crash.objects.aggregate(*[{"$match": query}])
-    return list(crashes)
+        query.update({'job_id': job_id})
+    return list(Crash.objects.aggregate(*[{'$match': query}]))
 
 
 def testcase_can_be_diffed(job_id):
-    mutation_engine = list(Job.objects(id=job_id))[0]["mutation_engine"]
-    if mutation_engine == "radamsa":
+    mutation_engine = list(Job.objects(id=job_id))[0]['mutation_engine']
+    if mutation_engine == 'radamsa':
         return True
     else:
         return False
@@ -134,11 +144,11 @@ def testcase_can_be_diffed(job_id):
 
 def get_original_and_crash_test_case_of_crash(crash):
     crash_test_case = crash.test_case
-    original_test_case = list(Job.objects(id=crash.job_id))[0]["samples"]
+    original_test_case = list(Job.objects(id=crash.job_id))[0]['samples']
     encoded_crash_test_case = base64.b64encode(crash_test_case).decode('ascii')
 
     if testcase_can_be_diffed(crash.job_id):
-        if (original_test_case.startswith(b'PK')):
+        if original_test_case.startswith(b'PK'):
             original_test_case = get_original_crash_test_case_of_zipfile(crash_test_case, original_test_case)
         encoded_original_test_case = base64.b64encode(original_test_case).decode('ascii')
     else:
@@ -164,33 +174,33 @@ def get_original_crash_test_case_of_zipfile(crash_test_case, original_test_case)
 @crashes.route('/crashes/show/next/<crash_id>')
 @login_required
 def show_next_crash(crash_id):
-    job_id_of_crash = list(Crash.objects(id=crash_id))[0]["job_id"]
+    job_id_of_crash = list(Crash.objects(id=crash_id))[0]['job_id']
     all_job_crashes = list(Crash.objects(job_id=job_id_of_crash))
     for index, crash in enumerate(all_job_crashes):
-        if str(crash["id"]) == str(crash_id):
+        if str(crash['id']) == str(crash_id):
             if index == len(all_job_crashes) - 1:
                 next_crash_index = 0
                 break
             else:
                 next_crash_index = index + 1
                 break
-    return show_crash(all_job_crashes[next_crash_index]["id"])
+    return show_crash(all_job_crashes[next_crash_index]['id'])
 
 
 @crashes.route('/crashes/show/previous/<crash_id>')
 @login_required
 def show_previous_crash(crash_id):
-    job_id_of_crash = list(Crash.objects(id=crash_id))[0]["job_id"]
+    job_id_of_crash = list(Crash.objects(id=crash_id))[0]['job_id']
     all_job_crashes = list(Crash.objects(job_id=job_id_of_crash))
     for index, crash in enumerate(all_job_crashes):
-        if str(crash["id"]) == str(crash_id):
+        if str(crash['id']) == str(crash_id):
             if index == 0:
                 next_crash_index = len(all_job_crashes) - 1
                 break
             else:
                 next_crash_index = index - 1
                 break
-    return show_crash(all_job_crashes[next_crash_index]["id"])
+    return show_crash(all_job_crashes[next_crash_index]['id'])
 
 
 @crashes.route('/crashes/download/<crash_id>')
@@ -208,5 +218,5 @@ def download_crash(crash_id):
             flash('You are not allowed to download this crash.')
             return redirect('crashes/show')
     else:
-        flash('Unknown crash ID: %s' % crash_id)
+        flash(f'Unknown crash ID: {crash_id}')
         return redirect('crashes/show')
